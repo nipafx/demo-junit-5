775bbd0192e5e5f7a30b32b001c4d554503d4b5f
parlog <nicolai.parlog@disy.net>
2016-02-25 10:15:43 +0100

    Demonstrate assumptions

---
+++ src/test/java/org/codefx/demo/junit5/Assume.java
@@ -0,0 +1,35 @@
+package org.codefx.demo.junit5;// NOT_PUBLISHED
+
+import org.junit.gen5.api.Test;
+
+import static org.junit.gen5.api.Assumptions.assumeFalse;
+import static org.junit.gen5.api.Assumptions.assumeTrue;
+import static org.junit.gen5.api.Assumptions.assumingThat;
+
+class Assume {
+
+	@Test
+	void exitIfFalseIsTrue() {
+		assumeTrue(false);
+		System.exit(1);
+	}
+	
+	@Test
+	void exitIfTrueIsFalse() {
+		assumeFalse(this::truism);
+		System.exit(1);
+	}
+	
+	private boolean truism() {
+		return true;
+	}
+	
+	@Test
+	void exitIfNullEqualsString() {
+		assumingThat(
+				"null".equals(null),
+				() -> System.exit(1)
+		);
+	}
+
+}
