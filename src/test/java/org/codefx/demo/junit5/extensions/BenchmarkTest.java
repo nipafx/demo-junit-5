3045f74fea0b26d8261d2c8aaed17f781014bdd7
nipa <nipa@codefx.org>
2016-07-26 17:07:00 +0200

    Remove comment about failing extension
    See https://github.com/junit-team/junit5/issues/390.

--- src/test/java/org/codefx/demo/junit5/extensions/BenchmarkTest.java
+++ src/test/java/org/codefx/demo/junit5/extensions/BenchmarkTest.java
@@ -16,13 +16,6 @@ class BenchmarkTest {
 	@Test
 	@Benchmark
 	void benchmarked() throws InterruptedException {
-
-		/*
-		 * TODO find out why the extension fails.
-		 * Looks like `BenchmarkExtension.writeCurrentTime` and `BenchmarkExtension.readCurrentTime` get called twice
-		 * for one test method but the first read access removes the launch time so the second fails.
-		 */
-
 		Thread.sleep(100);
 		assertTrue(true);
 	}
