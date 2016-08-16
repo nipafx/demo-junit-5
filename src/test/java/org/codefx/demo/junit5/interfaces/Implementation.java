58427c9bda2e85576ee7ea7a5450f36bd505a216
nipa <nipa@codefx.org>
2016-08-16 18:17:17 +0200

    Show that inherited default test methods are executed

---
+++ src/test/java/org/codefx/demo/junit5/interfaces/Implementation.java
@@ -0,0 +1,7 @@
+package org.codefx.demo.junit5.interfaces;
+
+public class Implementation implements Interface {
+
+	// when run with JUnit Jupiter this class executes the inherited default test methods
+
+}
---
+++ src/test/java/org/codefx/demo/junit5/interfaces/Interface.java
@@ -0,0 +1,14 @@
+package org.codefx.demo.junit5.interfaces;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public interface Interface {
+
+	@Test
+	default void test() {
+		assertFalse(true);
+	}
+
+}
