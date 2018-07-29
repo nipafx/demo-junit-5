9dcb26ec5f0dfa9c78512999c499dc35f1572616
Nicolai Parlog <nipa@codefx.org>
2018-07-29 22:08:29 +0200

    Demonstrate repeated tests

---
+++ src/test/java/org/codefx/demo/junit5/templates/RepeatedInvocationTest.java
@@ -0,0 +1,25 @@
+package org.codefx.demo.junit5.templates;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RepeatedInvocationTest {
+
+	private static int REPETITION_COUNT = 0;
+
+	@DisplayName("Calling repeated...")
+	@RepeatedTest(
+			value = 5,
+			name = "... {currentRepetition}th "
+					+ "of {totalRepetitions} times")
+	void repeated(RepetitionInfo repetitions) {
+		REPETITION_COUNT++;
+		// RepetitionInfo::getCurrentRepetition starts with 1
+		assertThat(repetitions.getCurrentRepetition()).isEqualTo(REPETITION_COUNT);
+		assertThat(repetitions.getTotalRepetitions()).isEqualTo(5);
+	}
+
+}
